f2749f516ba93e30c680bfe272b986ad442e262a
drkwng <iamdrkwng@gmail.com>
2021-12-05 23:35:54 +0200

    lesson 5 fixes after mentor comments

--- news/views.py
+++ news/views.py
@@ -3,35 +3,35 @@ from django.shortcuts import render
 
 def index_handler(request):
     context = {}
-    return render(request, 'news/index.html', context)
+    return render(request, 'index.html', context)
 
 
 def about_handler(request):
     context = {}
-    return render(request, 'news/about.html', context)
+    return render(request, 'about.html', context)
 
 
 def contact_handler(request):
     context = {}
-    return render(request, 'news/contact.html', context)
+    return render(request, 'contact.html', context)
 
 
 def blog_handler(request):
     context = {}
-    return render(request, 'news/blog.html', context)
+    return render(request, 'blog.html', context)
 
 
 def category_handler(request):
     context = {}
-    return render(request, 'news/category.html', context)
+    return render(request, 'category.html', context)
 
 
 def single_handler(request):
     context = {}
-    return render(request, 'news/single.html', context)
+    return render(request, 'single.html', context)
 
 
 def robots_handler(request):
     context = {}
-    return render(request, 'news/robots.txt', context,
+    return render(request, 'robots.txt', context,
                   content_type='text/plain')
