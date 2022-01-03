85d499709025f565e835e37ce789556a1039c1c3
drkwng <SDFS23wsfSdfSD>
2022-01-03 15:58:40 +0200

    check_permissions command form userena

--- web_app/run.sh
+++ web_app/run.sh
@@ -4,5 +4,6 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py collectstatic --noinput
+python manage.py check_permissions
 
 gunicorn -b 0.0.0.0:8000 --reload -w 4 super_news.wsgi
