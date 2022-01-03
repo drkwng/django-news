bfe9ca9e3928da73cd557cbfda898723a9063511
drkwng <SDFS23wsfSdfSD>
2022-01-03 16:25:18 +0200

    check_permissions

--- web_app/run.sh
+++ web_app/run.sh
@@ -4,6 +4,7 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py collectstatic --noinput
-python manage.py check_permissions
 
 gunicorn -b 0.0.0.0:8000 --reload -w 4 super_news.wsgi
+
+python manage.py check_permissions
